18d67de5e33f07bafaa9a770df8f564dddb392e1
Benno Ommerborn <Benno@ommerborn.com>
2015-10-03 23:10:54 +0200

    Implement HTTP API

--- iOS/Notification/AppDelegate.swift
+++ iOS/Notification/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-
+        NSUserDefaults.standardUserDefaults().setObject(NSString(UTF8String: "LEON.BERNARD@AXIOMC.COM"), forKey: "username")
+        NSUserDefaults.standardUserDefaults().setObject(NSString(UTF8String: "axiom"), forKey: "password")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+        if let token = NSUserDefaults.standardUserDefaults().objectForKey("auth_token") as? NSString {
+            print("Auth token stored: \(token)")
+        }
+        
         let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
         UIApplication.sharedApplication().registerUserNotificationSettings(settings)
         UIApplication.sharedApplication().registerForRemoteNotifications()
@@ -24,7 +33,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject], fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
         print("Recvd not \(userInfo)")
-        completionHandler(UIBackgroundFetchResult.NoData)
+        
+        if let auth_token = NSUserDefaults.standardUserDefaults().objectForKey("auth_token") as? NSString {
+            Alamofire.request(.POST, getURL() + "/dispatch_push", parameters: ["token" : auth_token, "user_info" : userInfo])
+                    .responseJSON { request, response, result in
+                        print(response)
+
+                        completionHandler(UIBackgroundFetchResult.NewData)
+                        print("Called completion handler....")
+                }
+        }
+        else {
+            completionHandler(UIBackgroundFetchResult.NoData)
+            print("Called completion handler (no auth)....")
+        }
+
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -49,8 +72,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
+    func getURL() ->String{
+        return "http://172.27.3.49:5000"
+    }
+    
     func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
         print("Got token data\(deviceToken)")
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        if let username = defaults.objectForKey("username") as? NSString,
+            password = defaults.objectForKey("password") as? NSString {
+                Alamofire.request(.POST, getURL() + "/authenticate", parameters: ["username" : username, "password" : password, "push_token" : deviceToken.hexString()])
+                    .responseJSON { request, response, result in
+                        print(request)  // original URL request
+                        print(response) // URL response
+                        print(result)   // result of response serialization
+                        
+                        if let JSON = result.value {
+                            print("JSON: \(JSON)")
+                            NSUserDefaults.standardUserDefaults().setObject(JSON["token"], forKey: "auth_token")
+                            NSUserDefaults.standardUserDefaults().synchronize()
+                        }
+                }
+        }
     }
     
     func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
@@ -60,3 +104,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension NSData {
+    func hexString() -> String {
+        // "Array" of all bytes:
+        let bytes = UnsafeBufferPointer<UInt8>(start: UnsafePointer(self.bytes), count:self.length)
+        // Array of hex strings, one for each byte:
+        let hexBytes = bytes.map { String(format: "%02hhx", $0) }
+        // Concatenate all hex strings:
+        return hexBytes.joinWithSeparator("")
+    }
+}
